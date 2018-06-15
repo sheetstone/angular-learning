e37f19fe90cc83e1a757b73beca508745b82931b
PSG Laptop <Owner@psgs-mbp.acsrr.com>
2018-06-15 16:05:17 -0500

    Matrial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './home/welcome.component';
@@ -8,13 +9,18 @@ import { PageNotFoundComponent } from './pagenotfound/pagenotfound.component';
 import { ErrorpageComponent } from './errorpage/errorpage.component';
 import { ProductModule } from './products/product.module';
 import { AppRouterModule } from './config/app-router.module';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+
 
 @NgModule({
   imports: [
     BrowserModule,
     HttpClientModule,
     ProductModule,
-    AppRouterModule
+    AppRouterModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule
   ],
   declarations: [
     AppComponent,
