aa1bdc2b95302877a2c4eba4d2409c962198ec9b
Hong Zhang <hong.zhang3@xerox.com>
2018-06-27 16:42:44 -0500

    validation

--- src/app/customers/customer.module.ts
+++ src/app/customers/customer.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+// import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { CustomersComponent } from './customers.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule
+    ReactiveFormsModule
   ],
   declarations: [
     CustomersComponent
--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,17 +1,73 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, /*FormControl,*/ FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 
 import { Customer } from './customer';
+
+function emailMatcher(c: AbstractControl): { [key: string]: boolean } | null {
+  const emailControl = c.get('email');
+  const confirmControl = c.get('confirmEmail');
+  if (emailControl.pristine || confirmControl.pristine) {
+    return null;
+  }
+  if (emailControl.value === confirmControl.value) {
+    return null;
+  }
+
+  return { 'match': true };
+}
+
+function ratingRange(min: number, max: number): ValidatorFn {
+  return (c: AbstractControl): { [key: string]: boolean } | null => {
+    if (c.value !== undefined && (isNaN(c.value) || c.value < min || c.value > max)) {
+      return { 'range': true };
+    }
+    return null;
+  };
+}
 
 @Component({
   templateUrl: './customers.component.html',
   styleUrls: ['./customers.component.css']
 })
-export class CustomersComponent {
+export class CustomersComponent implements OnInit {
+  customerForm: FormGroup;
   customer: Customer = new Customer();
 
-  save(customerForm: NgForm) {
-    console.log(customerForm.form);
-    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    // User form builder
+    this.customerForm = this.fb.group({ // create Form group by using form builder
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.maxLength(50)]],
+      emailGroup: this.fb.group({
+        email: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+')]],
+        confirmEmail: ['', Validators.required]
+      }, { validator: emailMatcher }),
+      phone: '',
+      sendCatalog: true,
+      rating: ['', ratingRange(1, 5)],
+      notification: 'email'
+    });
+  }
+  save() {
+    console.log(this.customerForm);
+    console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+  populateTestData() {
+    this.customerForm.patchValue({
+      firstName: 'Jack',
+      lastName: 'Harkness',
+      sendCatalog: false
+    });
+  }
+  setNotification(notifyVar: string): void {
+    const phoneControl = this.customerForm.get('phone');
+    if (notifyVar === 'text') {
+      phoneControl.setValidators(Validators.required);
+    } else {
+      phoneControl.clearValidators();
+    }
+    phoneControl.updateValueAndValidity();
   }
 }
